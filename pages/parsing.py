62cfa483e4e7645772feb192e4b34c6d3ba3ed59
zontiq <cot65@yandex.ru>
2022-12-01 16:04:36 +0300

    Добавлен файл с методом парсинга данных, из локального excel файла

---
+++ pages/parsing.py
@@ -0,0 +1,34 @@
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Font, Alignment, PatternFill
+from os.path import join, abspath
+
+
+def working_with_table(file_name, tabs_number, string_tabl, column_tabl):
+    add_files = abspath(join('.', file_name))
+    # Настраиваем опции документа
+    file_option = load_workbook(filename=add_files, data_only=True, read_only=True)
+
+    # Получаем список с вкладками документа - sheetnames
+    tabs = list(file_option.sheetnames)
+    # Определяем нужную вкладку, икорректируем номер указания в функции. Зачем нам начинать с нуля? :)
+    target_tabs = tabs[tabs_number - 1]
+
+    # Обращаемся к нужной вкладке
+    employees_sheet = file_option[f'{target_tabs}']
+
+    list_value = list()
+    while employees_sheet.cell(string_tabl, column_tabl).value is not None:
+        list_value.append(employees_sheet.cell(string_tabl, column_tabl).value)
+        string_tabl += 1
+    return list_value
+
+
+car_type = working_with_table('ExLab_task.xlsx', 2, 3, 2)
+body_type = working_with_table('ExLab_task.xlsx', 2, 3, 3)
+car_brand = working_with_table('ExLab_task.xlsx', 2, 3, 4)
+fuel_for_car = working_with_table('ExLab_task.xlsx', 2, 3, 5)
+transmission = working_with_table('ExLab_task.xlsx', 2, 3, 6)
+car_color = working_with_table('ExLab_task.xlsx', 2, 3, 7)
+
+for i in range(len(car_type)):
+    print(car_type[i], '|', body_type[i], '|', car_brand[i], '|', fuel_for_car[i], '|', transmission[i], '|', car_color[i])
